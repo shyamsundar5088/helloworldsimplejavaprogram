9bd69a2e16cd1e3eb2e93bd180e0b2220daced13
shyamsundar5088 <158137166+shyamsundar5088@users.noreply.github.com>
2024-02-02 16:07:59 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch33 team with Devanshu , Nazeem  and Sopan");
+     System.out.println("Hello batch33 team with sundar , shyam  and Sopan");
       System.out.println("Hello Batch batch33 WITH FOR STUDENTS RUNNING in front of Harsha  and Mohit and Kumar iman in making CI automated vikas teach devops CI proecss  ");
     }  
 } 
